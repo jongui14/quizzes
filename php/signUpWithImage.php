97e29912f40a663207ccf1abad645b573220b301
jongui14 <31937957+jongui14@users.noreply.github.com>
2017-11-30 18:02:31 +0100

    Update signUpWithImage.php

--- php/signUpWithImage.php
+++ php/signUpWithImage.php
@@ -1,26 +1,30 @@
 <?php
-include 'configEzarri.php';
 
-$eposta = trim($_POST['eposta']);
-$deitura= trim($_POST['deitura']);
-$nick= trim($_POST['nick']);
-$pasahitza= trim($_POST['pasahitza1']);
-$argazkia=addslashes (file_get_contents($_FILES['igoIrudia']['tmp_name']));
 
-$sql = "INSERT INTO users(eposta,deitura,nick,pasahitza,argazkia)
-		VALUES('$_POST[eposta]','$_POST[deitura]','$_POST[nick]','$_POST[pasahitza1]','$argazkia')";
+	include 'configEzarri.php';
+	$eposta = trim($_POST['eposta']);
+	$deitura= trim($_POST['deitura']);
+	$nick= trim($_POST['nick']);
+	$pasahitza= trim($_POST['pasahitza1']);
+	$argazkia=addslashes (file_get_contents($_FILES['igoIrudia']['tmp_name']));
+		
+	$kodea='websistemak';
+	$pass=crypt($pasahitza,$kodea);
+
+	$sql = "INSERT INTO users(eposta,deitura,nick,pasahitza,argazkia)
+		VALUES('$_POST[eposta]','$_POST[deitura]','$_POST[nick]','$pass','$argazkia')";
 
 		
-if(!$niremysqli->query($sql)){
-	die('Errorea: ' . $niremysqli->error);
-	echo "Errorea gertatu da!";
-	echo "<a href='./signUp.html'>Berriro saiatu.</a>";
-}
-echo "Erabiltzaile bat gehitu da!";
-echo "<p><a href='../layout.html'>Hasiera</a>";
-echo "<p><a href='../php/singUp.html'>Beste erabiltzaile bat gehitu</a>";
+	if(!$niremysqli->query($sql)){
+		die('Errorea: ' . $niremysqli->error);
+		echo ("Errorea gertatu da!");
+		echo ("<a href='./signUp.html'>Berriro saiatu.</a>");
+	}
+	
+	echo "Erabiltzaile bat gehitu da!";
+	echo "<p><a href='./layoutR.php'>Hasiera</a>";
+	echo "<p><a href='./signUp.php'>Beste erabiltzaile bat gehitu</a>";
 
-
-$niremysqli->close();
+	$niremysqli->close();	
 	
 ?>
